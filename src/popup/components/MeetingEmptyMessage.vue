613c318e3e4a54ca3e7f7454096d54976582d079
moromi25 <96011806+moromi25@users.noreply.github.com>
2024-01-15 01:42:06 +0900

    refs #8 計測済みのミーティングがない場合のui改善 (#9)

--- src/popup/components/MeetingEmptyMessage.vue
+++ src/popup/components/MeetingEmptyMessage.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="empty-meeting-row">ミーティング記録がありません</div>
+  <div class="empty-meeting-message-area">
+    <font-awesome-icon class="icon-empty-meeting" icon="fa-solid fa-stopwatch" />
+    <div class="empty-meeting-title">ミーティング記録がありません</div>
+  </div>
 </template>
 
 <script>
@@ -10,9 +13,20 @@ export default {
 </script>
 
 <style scoped>
-.empty-meeting-row {
+.empty-meeting-message-area {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.icon-empty-meeting {
+  font-size: 128px;
+  color: #a9a9a9;
+}
+
+.empty-meeting-title {
   color: #7f7f7f;
-  font-size: 16px;
+  font-size: 20px;
   margin-top: 16px;
 }
 </style>
